21506d070f8d7586bb9d8951a89e71e5e17096cb
crotten <wildrunts@gmail.com>
2014-08-06 16:38:39 -0400

    Working on the DOM

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ function Athlete(name, sport, speed) {
   this.eat = function(food) {
     var speedWeight = this.speed + food.weight;
     if (speedWeight > 110) {
-    console.log("Damn you are fast!")
+    console.log("Damn you are fast!");
   }
     else if(speedWeight > 70 && speedWeight < 110){
-      console.log("You may want to eay something")
+      console.log("You may want to eay something");
     }
     else {
     console.log("You really need to eat something!");
   }
+  this.speed += food.weight
   };
 
 }
@@ -26,13 +27,13 @@ function Food(item, weight, effect) {
   this.cause = function(athlete) {
     var whatDidYouEat = athlete.name +" is "+ this.effect + "!";
     if (this.effect === "Sick") {
-      console.log(whatDidYouEat)
+      console.log(whatDidYouEat);
     } else if (this.effect === "Healthy") {
-      console.log(whatDidYouEat)
+      console.log(whatDidYouEat);
     } else if (this.effect === "Full of toxins") {
-      console.log(whatDidYouEat)
+      console.log(whatDidYouEat);
     }
-  }
+  };
 
 
 
@@ -45,24 +46,66 @@ function Shoe(brand, endurance) {
   this.footwear = function(athlete) {
     var footwearPerformance = this.endurance + athlete.speed;
     if (footwearPerformance > 95){
-      console.log("You picked the right footwear!")
+      console.log("You picked the right footwear!");
     } else {
       console.log("You may want to pick another footwear");
     }
 
 
-  }
+  };
 
 }
 
+var Chris = new Athlete("Chris", "Frisbee", 65);
+var Bolt = new Athlete("Bolt", "Running", 95);
+var Peterson = new Athlete("Peterson", "Football", 90);
+var Athletes = [Chris, Bolt, Peterson];
 
-var Phelps = new Athlete("Phelps", "Swimming", 85)
-var Bolt = new Athlete("Bolt", "Running", 95)
-var Peterson = new Athlete("Peterson", "Football", 90)
+var IceCream = new Food("IceCream", -40, "Sick");
+var Protein = new Food("Protein", 10, "Healthy");
+var RedBull = new Food("RedBull", 50, "Full of toxins");
 
-var IceCream = new Food("IceCream", -40, "Sick")
-var Protein = new Food("Protein", 10, "Healthy")
-var RedBull = new Food("RedBull", 50, "Full of toxins")
+var Nike = new Shoe("Nike", 20);
+var Puma = new Shoe("Puma", 30);
+var Sandal = new Shoe("Sandal", -15);
 
-var Trainer = new Shoe("Trainer", 20)
-var Sandal = new Shoe("Sandal", -15)
+$(document).ready(function() {
+  page.init();
+});
+
+var page = {
+
+  init: function() {
+    this.initStyling();
+    this.initEvents();
+  },
+  initStyling: function() {
+
+
+  },
+  initEvents: function() {
+    $(".select_athlete").on("click", ".Chris", function() {
+      Chris;
+      console.log(Chris);
+    });
+
+    $(".select_athlete").on("click", ".Peterson", function() {
+      Peterson;
+      console.log(Peterson);
+    });
+
+    $(".select_athlete").on("click", ".Bolt", function() {
+      Bolt;
+      console.log(Bolt);
+    });
+
+    $(".fooditem").on("click", ".RedBullFood", function() {
+      Athletes.name.eat(RedBull);
+      console.log("Redbull");
+
+  });
+
+
+  }
+
+}
